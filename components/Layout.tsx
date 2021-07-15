90591d8ee7cb0aef274a0ce3d7996f07d58d0320
Youpele Michael <mjayjesus@gmail.com>
2021-07-16 00:53:04 +0200

    mobile responsiveness improvement

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -3,11 +3,11 @@ import Head from 'next/head'
 
 const Layout = ({ title, children }) => {
   return (
-    <div className='bg-gray-300'>
+    <div className='bg-gray-300 '>
       <Head>
         <title>{title}</title>
       </Head>
-      <main className='container mx-auto max-w-xl pt-8 min-h-screen'>
+      <main className='container mx-auto max-w-xl pt-8 min-h-screen  '>
         {children}
       </main>
     </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,52 +8,54 @@ export default function Home({ pokemon }) {
   // console.log(pokemon)
   return (
     <Layout title='Pokedex'>
-      <div className=' mb-8'>
-        <h1 className='text-7xl  font-bold text-center'>
-          <Image
-            src='/Pokeball.svg'
-            alt=''
-            className='animate-bounce'
-            height={100}
-            width={100}
-          />
-          Pokedex
-          <Image
-            src='/Pokeball.svg'
-            alt=''
-            className='animate-bounce'
-            height={100}
-            width={100}
-          />
-        </h1>
+      <div className='px-6'>
+        <div className=' mb-8'>
+          <h1 className='text-7xl   font-bold text-center px-0 mx-0'>
+            <Image
+              src='/Pokeball.svg'
+              alt=''
+              className='animate-bounce inline-block'
+              height={100}
+              width={100}
+            />
+            <div className='inline-block'>Pokedex</div>
+            <Image
+              src='/Pokeball.svg'
+              alt=''
+              className='animate-bounce inline-block'
+              height={100}
+              width={100}
+            />
+          </h1>
+        </div>
+        <ul>
+          {pokemon.map((pokeman, index) => {
+            return (
+              <li
+                key={index}
+                className='hover:font-bold hover:scale-125 hover:m-7 '
+              >
+                <Link href={`/pokemon/${index + 1}`}>
+                  <a
+                    className='border p-4 border-gray-200 my-2 capitalize flex items-center text-lg bg-gray-200 rounded-3xl '
+                    href={pokeman.url}
+                  >
+                    <Image
+                      height={100}
+                      width={100}
+                      className='w-20 h-20 mr-3'
+                      src={pokeman.image}
+                      alt={pokeman.name}
+                    />
+                    <span className='mr-2 font-bold'>{index + 1}</span>
+                    {pokeman.name}
+                  </a>
+                </Link>
+              </li>
+            )
+          })}
+        </ul>
       </div>
-      <ul>
-        {pokemon.map((pokeman, index) => {
-          return (
-            <li
-              key={index}
-              className='hover:font-bold hover:scale-125 hover:m-7 '
-            >
-              <Link href={`/pokemon/${index + 1}`}>
-                <a
-                  className='border p-4 border-gray-200 my-2 capitalize flex items-center text-lg bg-gray-200 rounded-3xl '
-                  href={pokeman.url}
-                >
-                  <Image
-                    height={100}
-                    width={100}
-                    className='w-20 h-20 mr-3'
-                    src={pokeman.image}
-                    alt={pokeman.name}
-                  />
-                  <span className='mr-2 font-bold'>{index + 1}</span>
-                  {pokeman.name}
-                </a>
-              </Link>
-            </li>
-          )
-        })}
-      </ul>
     </Layout>
   )
 }
